db88c8370654c411b7053e83f43cc8afdb299251
raj-kartik <rajkartik159@gmail.com>
2025-05-13 15:05:07 +0530

    partial commiting and pushing

--- screens/Stack/App/AccountScreen/Studio/StudioDetail.tsx
+++ screens/Stack/App/AccountScreen/Studio/StudioDetail.tsx
@@ -1,75 +1,119 @@
-import { Image, Linking, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import CustomText from '@components/Customs/CustomText'
-import Container from '@components/Container'
-import CustomHeader2 from '@components/Customs/Header/CustomHeader2'
-import Images from '@utils/Images'
-import { moderateScale } from '@components/Matrix/Matrix'
-import { globalStyle } from '@utils/GlobalStyle'
-import Colors from '@utils/Colors'
-import CustomIcon from '@components/Customs/CustomIcon'
-import SubHeader from '@components/Customs/Header/SubHeader1'
+import {
+    Image,
+    Linking,
+    Pressable,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import React from 'react';
+import CustomText from '@components/Customs/CustomText';
+import Container from '@components/Container';
+import CustomHeader2 from '@components/Customs/Header/CustomHeader2';
+import Images from '@utils/Images';
+import { moderateScale, screenWidth } from '@components/Matrix/Matrix';
+import { globalStyle } from '@utils/GlobalStyle';
+import Colors from '@utils/Colors';
+import CustomIcon from '@components/Customs/CustomIcon';
+import SubHeader from '@components/Customs/Header/SubHeader1';
 
 const StudioDetail = (props: any) => {
-
-    console.log("item in the studio detail", props?.route?.params?.item);
+    console.log('item in the studio detail', props?.route?.params?.item);
     const { item } = props?.route?.params;
 
     return (
-        <Container>
-            <CustomHeader2 title="Studio Detail" />
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-            >
-                <View style={[styles.imageView, globalStyle.center]} >
-                    {
-                        item?.image ? (
-                            <Image source={{ uri: item?.image }} style={{
+        <View style={{ flex: 1, backgroundColor: "#fff", }} >
+            <StatusBar
+                backgroundColor={Colors.orange_bg}
+            />
+            <CustomHeader2 title="Studio Detail" bg={Colors.orange_bg} />
+            <ScrollView showsVerticalScrollIndicator={false} >
+                <View style={[styles.imageView, globalStyle.center]}>
+
+                    <View
+                        style={styles.imageDesign}
+                    />
+                    {item?.image ? (
+                        <Image
+                            source={{ uri: item?.image }}
+                            style={{
                                 height: moderateScale(140),
                                 width: moderateScale(140),
                                 borderRadius: moderateScale(140),
-                            }} />
-                        ) : (
-                            <Images.Coin width={moderateScale(120)} height={moderateScale(120)} style={{
-                                height: moderateScale(150),
-                            }} />
-                        )
-                    }
-
-
-                    <View style={[globalStyle.center, {
-                        width: moderateScale(100),
-                        height: moderateScale(30),
-                        // height:moderateScale(20),
-                        padding: moderateScale(5),
-                        backgroundColor: Colors.orange_bg,
-                        borderRadius: moderateScale(100),
-                        position: "absolute",
-                        bottom: -10,
-                        right: -moderateScale(20),
-
-                    }]} >
-                        <CustomText
-                            text={item?.studio_type}
-                            weight='500'
+                            }}
                         />
+                    ) : (
+                        <Images.Coin
+                            width={moderateScale(120)}
+                            height={moderateScale(120)}
+                            style={{
+                                height: moderateScale(150),
+                            }}
+                        />
+                    )}
+
+                    <View
+                        style={[
+                            globalStyle.center,
+                            {
+                                width: moderateScale(100),
+                                height: moderateScale(30),
+                                // height:moderateScale(20),
+                                padding: moderateScale(5),
+                                backgroundColor: Colors.orange_bg,
+                                borderRadius: moderateScale(100),
+                                position: 'absolute',
+                                bottom: -10,
+                                right: -moderateScale(20),
+                            },
+                        ]}>
+                        <CustomText text={item?.studio_type} weight="500" />
                     </View>
                 </View>
 
                 {/* contact + email */}
-                <View style={{ alignSelf: "center", }} >
-                    <CustomText text={item?.studio_name} size={22} weight='600' customStyle={{ textAlign: 'center', marginTop: moderateScale(5) }} />
-                    <View style={[globalStyle.center, { width: "100%", alignSelf: "center", marginVertical: moderateScale(10), flexDirection: "row" }]} >
+                <View style={{ alignSelf: 'center',paddingHorizontal:moderateScale(10) }}>
+                    <CustomText
+                        text={item?.studio_name}
+                        size={22}
+                        weight="600"
+                        customStyle={{ textAlign: 'center', marginTop: moderateScale(5) }}
+                    />
+                    <View
+                        style={[
+                            globalStyle.center,
+                            {
+                                width: '100%',
+                                alignSelf: 'center',
+                                marginVertical: moderateScale(10),
+                                flexDirection: 'row',
+                            },
+                        ]}>
                         <Pressable
                             onPress={() => {
                                 if (item?.contact_number) {
                                     Linking.openURL(`tel:${item.contact_number}`);
                                 }
                             }}
-                            style={[globalStyle.row, { alignSelf: "center", flex: 1, justifyContent: "center", alignItems: "center" }]}
-                        >
-                            <CustomIcon type='Feather' name='phone' color={Colors.orange} />
-                            <CustomText customStyle={{ marginLeft: moderateScale(5) }} text={`+91-${item?.contact_number}`} size={18} weight='500' color={Colors.gray_font} />
+                            style={[
+                                globalStyle.row,
+                                {
+                                    alignSelf: 'center',
+                                    flex: 1,
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                },
+                            ]}>
+                            <CustomIcon type="Feather" name="phone" color={Colors.orange} />
+                            <CustomText
+                                customStyle={{ marginLeft: moderateScale(5) }}
+                                text={`+91-${item?.contact_number}`}
+                                size={15}
+                                weight="500"
+                                color={Colors.gray_font}
+                            />
                         </Pressable>
 
                         <Pressable
@@ -78,74 +122,96 @@ const StudioDetail = (props: any) => {
                                     Linking.openURL(`mailto:${item.email}`);
                                 }
                             }}
-                            style={[globalStyle.row, { alignSelf: "center", flex: 1, justifyContent: "center", alignItems: "center" }]}
-                        >
-                            <CustomIcon type='Feather' name='mail' color={Colors.orange} />
-                            <CustomText customStyle={{ marginLeft: moderateScale(5) }} text={item?.email} size={18} weight='500' color={Colors.gray_font} />
+                            style={[
+                                globalStyle.row,
+                                {
+                                    alignSelf: 'center',
+                                    flex: 1,
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                },
+                            ]}>
+                            <CustomIcon type="Feather" name="mail" color={Colors.orange} />
+                            <CustomText
+                                customStyle={{ marginLeft: moderateScale(5) }}
+                                text={item?.email}
+                                size={15}
+                                weight="500"
+                                color={Colors.gray_font}
+                            />
                         </Pressable>
                     </View>
 
                     {/* location */}
-                    <View style={[globalStyle.center, { alignItems: "center", width: "100%", flexDirection: "row" }]} >
+                    <View
+                        style={[
+                            globalStyle.center,
+                            { alignItems: 'center', width: '100%', flexDirection: 'row' },
+                        ]}>
                         <CustomIcon
-                            type='Ionicons'
-                            name='location-sharp'
+                            type="Ionicons"
+                            name="location-sharp"
                             color={Colors.orange}
                         />
-                        <CustomText
-                            text={item?.location}
-                            size={18}
-                            weight='500'
-                        />
+                        <CustomText text={item?.location} size={18} weight="500" />
                     </View>
                 </View>
 
-                <View style={[globalStyle.betweenCenter, { marginTop: moderateScale(10), justifyContent: "space-evenly" }]} >
-
+                <View
+                    style={[
+                        globalStyle.betweenCenter,
+                        { marginTop: moderateScale(10), justifyContent: 'space-evenly',paddingHorizontal:moderateScale(10) },
+                    ]}>
                     {/* opens at */}
-                    <View style={[globalStyle.row]} >
+                    <View style={[globalStyle.center, styles.timingContainer, { marginHorizontal: moderateScale(5) }]}>
                         <CustomIcon
-                            type='MaterialCommunityIcons'
-                            name='clock-time-four-outline'
+                            type="MaterialCommunityIcons"
+                            name="clock-time-four-outline"
                             color={Colors.orange}
                             size={30}
                         />
-                        <View style={{ marginLeft: moderateScale(5) }} >
-                            <CustomText text='Opens at' weight='500' color={Colors.gray} />
-                            <CustomText text={item?.opening_time.slice(0, 5)} weight='600' />
+                        <View style={{ marginLeft: moderateScale(5) }}>
+                            <CustomText text="Opens at" weight="500" color={Colors.gray} />
+                            <CustomText text={item?.opening_time.slice(0, 5)} weight="600" />
                         </View>
                     </View>
 
                     {/* closes at */}
-                    <View style={[globalStyle.row]} >
+                    <View
+                        style={[
+                            globalStyle.center,
+                            styles.timingContainer,
+                            {
+                                marginHorizontal: moderateScale(5),
+                            },
+                        ]}>
                         <CustomIcon
-                            type='MaterialCommunityIcons'
-                            name='clock-time-nine-outline'
+                            type="MaterialCommunityIcons"
+                            name="clock-time-nine-outline"
                             color={Colors.orange}
                             size={30}
                         />
-                        <View style={{ marginLeft: moderateScale(5) }} >
-                            <CustomText text='Opens at' weight='500' color={Colors.gray} />
-                            <CustomText text={item?.closing_time.slice(0, 5)} weight='600' />
+                        <View style={{ marginLeft: moderateScale(5) }}>
+                            <CustomText text="Opens at" weight="500" color={Colors.gray} />
+                            <CustomText text={item?.closing_time.slice(0, 5)} weight="600" />
                         </View>
                     </View>
                 </View>
 
                 {/* courses */}
-                <View style={{ marginTop: moderateScale(10) }} >
+                <View style={{ marginTop: moderateScale(10),paddingHorizontal:moderateScale(10) }}>
                     <SubHeader title="Courses" isMore={true} />
                 </View>
-
             </ScrollView>
-        </Container>
-    )
-}
+        </View>
+    );
+};
 
-export default StudioDetail
+export default StudioDetail;
 
 const styles = StyleSheet.create({
     imageView: {
-        alignSelf: "center",
+        alignSelf: 'center',
         // backgroundColor:"red",
         borderRadius: moderateScale(200),
         borderWidth: 2,
@@ -153,6 +219,26 @@ const styles = StyleSheet.create({
         borderColor: Colors.orange,
         width: moderateScale(150),
         height: moderateScale(150),
-        marginBottom: moderateScale(15)
+        marginBottom: moderateScale(15),
+    },
+    timingContainer: {
+        backgroundColor: Colors.orange_bg,
+        flex: 1,
+        // marginRight: moderateScale(5),
+        flexDirection: 'row',
+        padding: moderateScale(10),
+        borderRadius: moderateScale(5),
+        elevation: 5,
+        shadowColor: Colors.text_orange
+    },
+    imageDesign: {
+        width: screenWidth * 1.2,
+        alignSelf: "center",
+        zIndex: -1,
+        backgroundColor: Colors.orange_bg,
+        height: 400,
+        position: "absolute",
+        top: -280,
+        borderRadius: moderateScale(300)
     }
-})
+});
